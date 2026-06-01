c4bc8c9f5393032544046b843c8574c0bd6b5e3e
victor-melo-master <victor@MacBook-Pro-de-Victor.local>
2026-06-01 19:00:28 -0400

    chore: eliminar frontend Flutter (deprecated, sin uso en producción)

--- app/android/app/src/main/kotlin/com/casacambio/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.casacambio.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- app/lib/core/constants/api_constants.dart
+++
@@ -1,3 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'https://intermedius.crececrm.com/api/v1';
-}
